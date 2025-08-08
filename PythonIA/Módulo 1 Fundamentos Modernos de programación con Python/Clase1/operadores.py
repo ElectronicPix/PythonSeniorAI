44bf423edb71ffec55b846733cc6d80bb0578ea4
Rubén De La Puente <rubendlpc@gmail.com>
2025-08-07 20:08:19 -0500

    Add arithmetic operations example and update images
    Added a commented example of basic arithmetic operations in operadores.py. Added a new image 'Python POO.PNG' and moved 'image.png' to a new location for better organization.

--- PythonIA/Módulo 1 Fundamentos Modernos de programación con Python/Clase1/operadores.py
+++ PythonIA/Módulo 1 Fundamentos Modernos de programación con Python/Clase1/operadores.py
@@ -1,5 +1,24 @@
+'''
 #declarar variables y pedir datos por teclado
-num1 = float(input("Ingrese el numero 1: "))
+num1 = float(input(" Ingrese el numero 1: "))
 num2 = float(input("Ingrese el numero 2: "))
 
-#implementar oeradores
+#implementar oeradores (aritmeticos) = operador de asignacion
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+potencia = num1 ** num2
+modulo = num1 % num2    
+division_entera = num1 // num2
+
+#salida de datos por consola
+print(f"SUMA: {suma:.0f}")
+print(F"RESTA: {resta}")
+print(f"MULTIPLICACION: {multiplicacion}")
+print(f"DIVISION: {division}")
+print(f"POTENCIA: {potencia}")
+print(f"MODULO: {modulo}")
+print(f"DIVISION ENTERA: {division_entera}")
+'''
+
